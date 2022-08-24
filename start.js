c150a6a98ab086f847afbe0e55efa20099346a25
okjin <110460300+okjin1217@users.noreply.github.com>
2022-08-24 19:32:46 +0900

    Update start.js

--- start.js
+++ start.js
@@ -2,6 +2,15 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
 function begin(){
-  main.style.display = "none";
-  qna.style.display = "block";
+  main.style.WedkitAnimation = "fadeOut 1s";
+  main.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    qna.style.WedkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIn 1s";
+  setTimeout(() => {
+    main.style.display = "none";
+    qna.style.display = "block";
+    }, 450)
+  }, 450);
 }
+
