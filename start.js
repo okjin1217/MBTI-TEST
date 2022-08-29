105e5bcdf0bfe5c38d73980bec45ddcd4367f1ca
okjin <110460300+okjin1217@users.noreply.github.com>
2022-08-29 19:47:51 +0900

    Update start.js

--- start.js
+++ start.js
@@ -1,42 +1,44 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
-function addAnswer(answerText, qIdx){
+
+
+function addAnswer(answerText, qIdx) {
   var a = document.querySelector('.answerBox');
   var answer = document.createElement('button');
-  answer.classList.add('answerList')
+  answer.classList.add('answerList');
   a.appendChild(answer);
   answer.innerHTML = answerText;
-  
-  answer.addEventListener("click", function(){
+
+  answer.addEventListener("click", function() {
     var children = document.querySelectorAll('.answerList');
-      for(let i = 0; i < children.length; i++){
-        children[i].disabled = true;
-        children[i].style.display = 'none';
-      }
-     goNext(++qIdx);
-   }, false);
+    for (let i = 0; i < children.length; i++) {
+      children[i].disabled = true;
+      children[i].style.display = 'none';
+    }
+    goNext(++qIdx);
+  }, false);
 }
- function goNext(qIdx){
+
+function goNext(qIdx) {
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
-  for(let i in qnaList[qIdx].a){
-    addAnswer(qnaList[qIdx].a[i].answert, qIdx);
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
 }
 
-function begin(){
-  main.style.WedkitAnimation = "fadeOut 1s";
+function begin() {
+  main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
   setTimeout(() => {
-    qna.style.WedkitAnimation = "fadeIn 1s";
+    qna.style.WebkitAnimation = "fadeIn 1s";
     qna.style.animation = "fadeIn 1s";
-  setTimeout(() => {
-    main.style.display = "none";
-    qna.style.display = "block";
+    setTimeout(() => {
+      main.style.display = "none";
+      qna.style.display = "block"
     }, 450)
     let qIdx = 0;
     goNext(qIdx);
   }, 450);
 }
-
